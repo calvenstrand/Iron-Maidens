55786c11fd39fb21ba1a4fd44432379f31faea88
Eric Ejneroos <eric@ejneroos.se>
2012-01-17 15:59:32 +0100

    Fixat ihop fra'n b'o'rjan, DraughtsBundle nu ist'a'llet, inga on'o'diga sidor osv... Funkar att l'a'gga till en databas.

---
+++ Draughts-Symfony/app/cache/dev/twig/a5/77/a25532da6bbae7056ddf108c6ed1.php
@@ -0,0 +1,112 @@
+<?php
+
+/* ::base.html.twig */
+class __TwigTemplate_a577a25532da6bbae7056ddf108c6ed1 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->blocks = array(
+            'title' => array($this, 'block_title'),
+            'stylesheets' => array($this, 'block_stylesheets'),
+            'body' => array($this, 'block_body'),
+            'form' => array($this, 'block_form'),
+            'javascripts' => array($this, 'block_javascripts'),
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<!-- app/Resources/views/base.html.twig -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv=\"Content-Type\" content=\"text/html\"; charset=utf-8\" />
+        <title>";
+        // line 6
+        $this->displayBlock('title', $context, $blocks);
+        echo " - symblog</title>
+        <!--[if lt IE 9]>
+            <script src=\"http://html5shim.googlecode.com/svn/trunk/html5.js\"></script>
+        <![endif]-->
+        ";
+        // line 10
+        $this->displayBlock('stylesheets', $context, $blocks);
+        // line 15
+        echo "        <link rel=\"shortcut icon\" href=\"";
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("favicon.ico"), "html", null, true);
+        echo "\" />
+    </head>
+    <body>
+\t
+\t\t<div id=\"mainGameWrapper\">
+\t\t";
+        // line 20
+        $this->displayBlock('body', $context, $blocks);
+        // line 21
+        echo "\t\t\t
+\t\t</div>
+\t\t";
+        // line 23
+        $this->displayBlock('form', $context, $blocks);
+        // line 24
+        echo "\t
+
+        ";
+        // line 26
+        $this->displayBlock('javascripts', $context, $blocks);
+        // line 29
+        echo "    </body>
+</html>";
+    }
+
+    // line 6
+    public function block_title($context, array $blocks = array())
+    {
+        echo "symblog";
+    }
+
+    // line 10
+    public function block_stylesheets($context, array $blocks = array())
+    {
+        // line 11
+        echo "            <link href='http://fonts.googleapis.com/css?family=Irish+Grover' rel='stylesheet' type='text/css'>
+            <link href='http://fonts.googleapis.com/css?family=La+Belle+Aurore' rel='stylesheet' type='text/css'>
+            <link href=\"";
+        // line 13
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("css/screen.css"), "html", null, true);
+        echo "\" type=\"text/css\" rel=\"stylesheet\" />
+        ";
+    }
+
+    // line 20
+    public function block_body($context, array $blocks = array())
+    {
+        echo " ";
+    }
+
+    // line 23
+    public function block_form($context, array $blocks = array())
+    {
+    }
+
+    // line 26
+    public function block_javascripts($context, array $blocks = array())
+    {
+        echo " 
+        
+        ";
+    }
+
+    public function getTemplateName()
+    {
+        return "::base.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+}
